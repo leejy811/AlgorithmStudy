f217402e251ef32acb9dc7572c9af63cd0560868
leejy811 <58072332+leejy811@users.noreply.github.com>
2023-06-27 18:08:56 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> wallpaper) {
+    vector<int> answer;
+    int maxX = 0, maxY = 0, minX = wallpaper[0].length()+1, minY = wallpaper.size()+1;
+    
+    for(int i=0;i<wallpaper.size();i++){
+        for(int j=0;j<wallpaper[i].length();j++){
+            if(wallpaper[i][j] == '#'){
+                if(j < minX) minX = j;
+                if(j+1 > maxX) maxX = j+1;
+                if(i < minY) minY = i;
+                if(i+1 > maxY) maxY = i+1;
+            }
+        }
+    }
+    
+    answer = {minY, minX, maxY, maxX};
+    return answer;
+}
