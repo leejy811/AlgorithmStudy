118531f36808ba784be9ef44e893245c6c73a07f
leejy811 <58072332+leejy811@users.noreply.github.com>
2024-01-15 13:27:54 +0900

    [level Lv.1] Title: 약수의 개수와 덧셈, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    
+    for(int i = left;i <= right;i++) {
+        answer += pow((int)sqrt(i), 2) == i ? i * -1 : i;
+    }
+    return answer;
+}
