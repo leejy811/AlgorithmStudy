96eabdb5f54e7bbaed24e184c7b4941d0339be97
leejy811 <58072332+leejy811@users.noreply.github.com>
2025-03-02 21:28:56 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(int argc, char const *argv[]) {
+    
+    int A, B;
+ 
+    cin >> A;
+    cin >> B;
+ 
+    cout << A - B;
+    return 0;
+}
