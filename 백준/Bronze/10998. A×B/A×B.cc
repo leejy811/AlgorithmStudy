0f5e4bb744e7bc1eccb1bec2e10a23aac9ccf4b8
leejy811 <58072332+leejy811@users.noreply.github.com>
2025-03-03 00:53:43 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0);
+    
+    int A, B;
+    
+    cin >> A;
+    cin >> B;
+    
+    cout << A*B;
+    
+    return 0;
+}
