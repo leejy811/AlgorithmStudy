9eb2af129da1151c20aa6fbdb198458ee736365a
leejy811 <58072332+leejy811@users.noreply.github.com>
2025-03-03 01:50:22 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0);
+    
+    string name;
+    
+    cin >> name;
+    
+    name += "??!";
+    
+    cout << name;
+    
+    return 0;
+}
