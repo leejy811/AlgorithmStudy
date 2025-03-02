1847f6932371685ae82ef360b68f4d3632ea0bb3
leejy811 <58072332+leejy811@users.noreply.github.com>
2025-03-03 01:03:27 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0);
+    
+    double A, B;
+    
+    cin >> A;
+    cin >> B;
+    
+    cout.precision(12);
+    cout << fixed;
+    cout << A / B;
+    
+    return 0;
+}
