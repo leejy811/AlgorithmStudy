1fe48595987cb15a3c29fbc6c5674f08b6386451
leejy811 <58072332+leejy811@users.noreply.github.com>
2025-03-04 18:23:43 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int num;
+    vector<int> res;
+
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        res.push_back(a + b);
+    }
+
+    for (int i = 0; i < num; i++)
+    {
+        cout << res[i] << "\n";
+    }
+
+    return 0;
+}
