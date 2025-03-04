ffd96bff809d8e79c4d5ea96730258a6d484f3dd
leejy811 <58072332+leejy811@users.noreply.github.com>
2025-03-04 18:45:46 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    vector<int> res;
+
+    while (1)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if (a == 0 && b == 0) break;
+
+        res.push_back(a + b);
+    }
+
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << "\n";
+    }
+
+    return 0;
+}
