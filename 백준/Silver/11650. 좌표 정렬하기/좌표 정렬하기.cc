90a1d88febb016b7e6cfbdc59de757891e4cf9f7
leejy811 <58072332+leejy811@users.noreply.github.com>
2025-03-12 15:55:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool cmp(pair<int, int> p1, pair<int, int> p2)
+{
+    if (p1.first == p2.first)
+        return p1.second < p2.second;
+    else
+        return p1.first < p2.first;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+
+    cin >> n;
+
+    vector<pair<int, int>> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+    }
+
+    sort(v.begin(), v.end(), cmp);
+
+    for (pair<int, int> p : v)
+    {
+        cout << p.first << " " << p.second << "\n";
+    }
+
+    return 0;
+}
