c0d03736d84f3fa9fa10e79c48ea8ef3c421af91
leejy811 <58072332+leejy811@users.noreply.github.com>
2025-03-23 15:12:30 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+
+    cin >> n;
+
+    vector<long long> v(n);
+    long long div = 10007;
+
+    v[0] = 1;
+    v[1] = 3;
+
+    for (int i = 2; i < n; i++)
+    {
+        v[i] = (v[i - 1] + v[i - 2] * 2) % 10007;
+    }
+
+    cout << v[n - 1];
+
+    return 0;
+}
