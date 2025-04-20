2fa365bb0eab2d8bde59bf2ea3e2dd9190897649
leejy811 <58072332+leejy811@users.noreply.github.com>
2025-04-20 19:09:30 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> v;
+vector<int> num;
+vector<bool> used;
+map<int, int> usedLoc;
+
+void func(int k)
+{
+    if (k == m)
+    {
+        for (int i = 0; i < m; i++)
+            cout << v[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    map<int, bool> usedLocal;
+    for (int i = 0; i < n; i++)
+    {
+        if (!used[i] && !usedLocal[num[i]])
+        {
+            v[k] = num[i];
+            used[i] = true;
+            usedLocal[num[i]] = true;
+            func(k + 1);
+            used[i] = false;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m;
+
+    v.resize(m);
+    num.resize(n);
+    used.resize(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> num[i];
+    }
+
+    sort(num.begin(), num.end());
+
+    func(0);
+
+    return 0;
+}
