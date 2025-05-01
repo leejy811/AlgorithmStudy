cb2fc214554cfac67d6ae9231d66f06400fc60cc
leejy811 <58072332+leejy811@users.noreply.github.com>
2025-05-02 02:26:48 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+
+    cin >> n;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            n /= i;
+            cout << i << '\n';
+        }
+    }
+
+    if (n != 1)
+        cout << n;
+
+    return 0;
+}
